91146f51a728e63bbdb7e25580ea2ab3850cd112
Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>
2019-11-07 09:30:13 +0200

    Update Android Gradle version 3.5.1 -> 3.5.2
    Signed-off-by: Oleksandr Bezushko <34242332+Insiderser@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,9 +53,9 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.1")
+        classpath("com.android.tools.build:gradle:3.5.2")
         classpath(kotlin("gradle-plugin", version = "1.3.50"))
-        
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
